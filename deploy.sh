b92b1d8a25abb3d3a49f68de71bccdf6d6f24fd5
Your Name <nathanielbd@gmail.com>
2020-06-18 17:10:35 -0500

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-#!/bin/sh
 
 # If a command fails then the deploy stops
 set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
 # Removing existing files
 rm -rf public/*
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
